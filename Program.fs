3d7f28d7e24c4ed4c180338601cb59703b37414b
Daniel Everland <danieleverland@gmail.com>
2021-10-08 09:40:32 +0200

    lfgkjdsfjg

--- Program.fs
+++ Program.fs
@@ -1,4 +1,6 @@
-﻿type Poly = int list
+﻿open FsCheck;;
+
+type Poly = int list
 
 let abs x =
     if x > 0 then x else x * -1
@@ -14,8 +16,14 @@ let rec mulC (c, xs) =
         | x::xs    -> (c * x)::mulC(c, xs)
         | _        -> xs
 
+let prune (xs) =
+    let result = List.tryFindIndexBack (fun x -> x <> 0) xs
+    match result with
+    | None -> []
+    | _ -> xs.[0..result.Value]
+
 let rec sub = function
-    | (x::xs, y::ys)   -> (x - y)::sub(xs, ys)
+    | (x::xs, y::ys)   -> prune((x - y)::sub(xs, ys))
     | ([], ys)         -> mulC(-1, ys)
     | (xs, [])         -> xs
 
@@ -30,10 +38,6 @@ let isLegal xs =
  match xs with
     | [] -> true
     | _ -> List.last xs <> 0
-
-let prune xs =
-    let i = List.findIndexBack (fun x -> x <> 0) xs
-    xs.[0..i]
 
 let toString (xs) =
     let rec doToString(i, ys) =
@@ -90,3 +94,36 @@ let addD (a, b) =
     match (a, b) with
     | (MinusInf, _) | (_, MinusInf) -> MinusInf
     | (Fin f, Fin g) -> Fin (f + g)
+
+// Tests:
+let addInv p1 p2 = isLegal(add( (prune p1), (prune p2) ));;
+let _ = Check.Quick addInv;;
+
+// 1
+let addAss p1 p2 p3 = add(add(p1, p2), p3) = add(p1, add(p2, p3));;
+let _ = Check.Quick addAss;;
+
+// 2
+let addCom p1 p2 = add(p1, p2) = add(p2, p1);;
+let _ = Check.Quick addCom;;
+
+// 3
+Check.Quick (fun p -> (add(p, []) = p) && add([], p) = p);;
+
+// 4
+Check.Quick (fun p -> sub(p, p) = [])
+
+// 5
+Check.Quick (fun p1 p2 p3 -> mul(mul(p1, p2), p3) = mul(p1, mul(p2, p3)));;
+
+// 6
+Check.Quick (fun p1 p2 -> mul(p1, p2) = mul(p2, p1))
+
+// 7
+Check.Quick (fun p -> mul(prune(p), [1]) = prune(p) && prune(p) = mul([1], prune(p)))
+
+// 8
+Check.Quick (fun p1 p2 p3 -> mul(p1, add(p2, p3)) = add(mul(p1, p2), mul(p1, p3)))
+
+// 9
+Check.Quick (fun p1 p2 p3 -> mul(add(prune(p1), prune(p2)), prune(p3)) = add(mul(prune(p1), prune(p3)), mul(prune(p2), prune(p3)))
